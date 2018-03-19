82239ec6d8207af0d1853e550d522953c3b40927
Jon Turner <theparticleman@gmail.com>
2018-03-18 20:18:33 -0600

    Initial code commit

---
+++ HttpSpheroColors/Index.cs
@@ -0,0 +1,29 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.ModelBinding;
+
+namespace HttpSpheroColors
+{
+    public class Index : NancyModule
+    {
+        private readonly string indexHtml;
+        public Index()
+        {
+            indexHtml = File.ReadAllText("Index.html");
+
+            Get["/"] = _ => indexHtml;
+
+            Post["/"] = parameters =>
+            {
+                var request = this.Bind<ColorRequest>();
+                Console.WriteLine($"Got a request with ({request.Red},{request.Green},{request.Blue})");
+                return indexHtml;
+            };
+        }
+    }
+}
---
+++ HttpSpheroColors/Program.cs
@@ -0,0 +1,28 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpSpheroColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HostConfiguration
+            {
+                UrlReservations = new UrlReservations { CreateAutomatically = true },
+                RewriteLocalhost = true
+            };
+            var uri = new Uri("http://localhost:80");
+            using (var nancy = new NancyHost(config, uri))
+            {
+                nancy.Start();
+                Console.WriteLine($"Listening at {uri}. Press enter to quit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
