8ce62da1b3855d4c933f7661360a9b9cf9acb034
Jon Turner <theparticleman@gmail.com>
2018-03-19 21:17:05 -0600

    Set different default color and add load script

---
+++ HttpSpheroColors/Load-Test.ps1
@@ -0,0 +1,8 @@
+﻿for ($i=0; $i -lt 300; $i++) {
+    $red = Get-Random -Minimum 0 -Maximum 256
+    $green = Get-Random -Minimum 0 -Maximum 256
+    $blue = Get-Random -Minimum 0 -Maximum 256
+
+    $body = "red=$red&green=$green&blue=$blue"
+    Invoke-WebRequest http://localhost -Method POST -Body $body
+}
